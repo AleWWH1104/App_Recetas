6f8c77f75710b64af0a6278d70c30e47a78ffb56
AleWWH1104 <irisale.ayala@gmail.com>
2025-02-09 11:48:03 -0600

    Composable de SignUp

--- app/src/main/java/com/my_recipes_app/app_recetas/ui/account/view/SignIn.kt
+++ app/src/main/java/com/my_recipes_app/app_recetas/ui/account/view/SignIn.kt
@@ -4,13 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
